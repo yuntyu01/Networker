d8d3d87211dbe08ff847498d68f1bb4caf845797
MiHyeon <76768575+MystyS2@users.noreply.github.com>
2024-07-22 17:06:17 +0900

    #41 비밀번호 유효성 검사

--- src/main/resources/static/src/js/deleteaccount.js
+++ src/main/resources/static/src/js/deleteaccount.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(response => {
             if (response.ok) {
-                return response.json();
+                return response.json(); //data = response.json() 탈퇴 성공 시 success 값이 true
             } else {
                 return response.json().then(errorData => {
                     throw new Error(errorData.message);
@@ -29,10 +29,11 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(data => {
             console.log('Response data:', data); // 디버깅을 위한 로그
+            // 로그인 성공시 성공 메세지 띄우고 홈 화면으로 이동
             if (data.success) {
                 alert(data.message);
                 window.location.href = 'networker.html';
-            } else {
+            } else { // 실패시 실패 메세지 알림
                 alert(data.message);
             }
         })
