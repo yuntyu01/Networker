c65d5fc2961fc38435bcfc122b8bdbe76176ada2
MiHyeon <76768575+MystyS2@users.noreply.github.com>
2024-07-22 10:16:00 +0900

    #29 로그인 여부에 따른 헤더 요소 변경 코드 추가

--- src/main/resources/static/src/js/post.js
+++ src/main/resources/static/src/js/post.js
@@ -6,6 +6,33 @@ document.addEventListener('DOMContentLoaded', async () => {
     const commentContent = document.getElementById('comment-content');
     const submitComment = document.getElementById('submit-comment');
 
+    // 로그인, 회원가입, 프로필 아이콘
+    const loginButton = document.querySelector('.auth-buttons a[href="login.html"]');
+    const signupButton = document.querySelector('.auth-buttons a[href="signup.html"]');
+    const profileIcon = document.querySelector('.auth-buttons .profile-icon');
+
+    // 로그인 상태 확인 함수(로그인 여부에 따라 헤더 요소 변경)
+    const checkLoginStatus = () => {
+        fetch('/board', {
+            method: 'GET',
+            credentials: 'include'
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.loggedIn) {
+                    loginButton.style.display = 'none';
+                    signupButton.style.display = 'none';
+                    profileIcon.style.display = 'inline-block';
+                } else {
+                    loginButton.style.display = 'inline-block';
+                    signupButton.style.display = 'inline-block';
+                    profileIcon.style.display = 'none';
+                }
+            })
+            .catch(error => console.error('Error:', error));
+    };
+
+
     // 게시글 내용 불러오기
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
     const post = await response.json();
@@ -53,4 +80,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         // 입력란 초기화
         commentContent.value = '';
     });
+
+
+    // 페이지 로드 시 로그인 상태 확인
+    checkLoginStatus();
+
 });
