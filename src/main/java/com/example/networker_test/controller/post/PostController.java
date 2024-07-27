56810dc22df6cec42454f22f438f46559a603299
chmin4 <chgkals@gmail.com>
2024-07-28 03:34:48 +0900

    Revert "Merge pull request #79 from KNUT-Hackathon/chgkals3"
    This reverts commit 414d2fc9954215bbcbf5178c1ff9b894388fa765, reversing
    changes made to b278ea4ba686ccf2f231a7126c007693ad1c6bd3.

--- src/main/java/com/example/networker_test/controller/post/PostController.java
+++ src/main/java/com/example/networker_test/controller/post/PostController.java
@@ -2,13 +2,8 @@ package com.example.networker_test.controller.post;
 
 import java.util.List;
 
-import com.example.networker_test.domain.comment.CommentForm;
 import com.example.networker_test.domain.post.Post;
 import com.example.networker_test.service.post.PostService;
-
-import jakarta.validation.Valid;
-
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,14 +22,14 @@ public class PostController {
 	private final PostService postService;//의존성 주입
 	
 	@GetMapping("/list")//게시물 리스트 페이지 연결
-	public String list(Model model, @RequestParam(value="page", defaultValue="1") int page) {
-		Page<Post> paging = this.postService.getList(page-1);//defaultValue="1"와 page-1는 프로그램 내부에서는 0부터 페이지를 세지만, 외부에서는 1부터 세는 것으로 보이게 하기 위함
-        model.addAttribute("paging", paging);
+	public String list(Model model) {
+        List<Post> postList = this.postService.getList();
+        model.addAttribute("postList", postList);
         return "board";
     }
 	
 	@GetMapping(value="/detail/{id}")//특정 게시물의 페이지 연결
-	public String detail(Model model, @PathVariable("id") Integer id, CommentForm commentForm) {
+	public String detail(Model model, @PathVariable("id") Integer id) {
     	Post post = this.postService.getPost(id);
     	model.addAttribute("post", post);
     	return "post";
