3f9dae49ed5573a980c831fb0d229051a70fb49e
chmin4 <chgkals@gmail.com>
2024-07-30 09:43:26 +0900

    commit_post&sessionfix

--- src/main/java/com/example/networker_test/repository/post/PostRepository.java
+++ src/main/java/com/example/networker_test/repository/post/PostRepository.java
@@ -1,9 +1,16 @@
 package com.example.networker_test.repository.post;
 
 import com.example.networker_test.domain.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Integer> {
-	
+    Post findBySubject(String subject);
+    Post findBySubjectAndContent(String subject, String content);
+    List<Post> findBySubjectLike(String subject);
+    Page<Post> findAll(Pageable pageable);
 
 }
