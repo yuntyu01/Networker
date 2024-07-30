96141703a049ac93086a300e6a6a8004a6546617
chmin4 <chgkals@gmail.com>
2024-07-30 20:50:53 +0900

    lawsupport

---
+++ src/main/java/com/example/networker_test/domain/post/PostLaw.java
@@ -0,0 +1,40 @@
+package com.example.networker_test.domain.post;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.example.networker_test.domain.comment.Comment;
+
+import com.example.networker_test.domain.comment.CommentLaw;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PostLaw {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length=100)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "postLaw", cascade = CascadeType.REMOVE)
+    private List<CommentLaw> commentLawList;
+
+
+}
+
---
+++ src/main/java/com/example/networker_test/repository/comment/CommentLawRepository.java
@@ -0,0 +1,10 @@
+package com.example.networker_test.repository.comment;
+
+import com.example.networker_test.domain.comment.Comment;
+import com.example.networker_test.domain.comment.CommentLaw;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentLawRepository extends JpaRepository<CommentLaw, Integer>{
+
+
+}
