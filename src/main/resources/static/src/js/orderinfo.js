32b01a63ed7793aecd920f25b71e23992284f8c6
MiHyeon <76768575+MystyS2@users.noreply.github.com>
2024-07-30 14:19:52 +0900

    #98 주문내역 없을 경우

--- src/main/resources/static/src/js/orderinfo.js
+++ src/main/resources/static/src/js/orderinfo.js
@@ -7,8 +7,13 @@ function fetchOrders() {
     fetch('/orderinfo')
         .then(response => response.json())
         .then(data => {
+            const emptyList = document.querySelector('.empty-list');
             const orderTableBody = document.getElementById('orderTableBody');
             data.forEach(info => {
+                // 데이터 없다는 텍스트 숨기기
+                emptyList.style.display = "none";
+
+                // 주문내역 출력
                 const row = document.createElement('tr');
                 row.classList.add('order-row');
                 row.dataset.orderId = info.orderId; // 주문 ID를 데이터 속성으로 저장
@@ -25,5 +30,8 @@ function fetchOrders() {
                 orderTableBody.appendChild(row);
             });
         })
-        .catch(error => console.error('Error fetching orders:', error));
+        .catch(error => {
+            emptyList.style.display = "block";
+            console.error('Error fetching orders:', error)
+        });
 }
