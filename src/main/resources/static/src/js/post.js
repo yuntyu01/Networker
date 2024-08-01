bd1cdfd602b1491096c2f7ea5e41544d618396f2
MiHyeon <76768575+MystyS2@users.noreply.github.com>
2024-08-01 22:06:01 +0900

    #107 js 코드 수정

--- src/main/resources/static/src/js/post.js
+++ src/main/resources/static/src/js/post.js
@@ -25,7 +25,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         })
         .then(response => response.json()) // 서버의 응답을 JSON 형태로 파싱합니다.
         .then(async data => {
-            
             // 로그인 상태를 확인합니다.
             if (data.loggedIn) {
                 // 로그인 상태인 경우
@@ -37,24 +36,16 @@ document.addEventListener('DOMContentLoaded', async () => {
                 const postuserId = await getPostuserId(); 
 
                 // 서버로 현재 로그인된 사용자의 id와 post id를 전송하여 비교 요청을 보냅니다.
-                const userId = data.userId; // 서버에서 받아온 현재 로그인된 사용자의 id
+                const userId = data.userEmail; // 서버에서 받아온 현재 로그인된 사용자의 id
 
-                fetch(`/compare-ids?postId=${postuserId}&userId=${userId}`, {
-                    method: 'GET', // 요청 메서드는 GET입니다.
-                    credentials: 'include' // 쿠키를 포함하여 요청을 보냅니다.
-                })
-                .then(response => response.json()) // 서버의 응답을 JSON 형태로 파싱합니다.
-                .then(result => {
-                    // 서버에서 받은 결과를 확인하여 post id와 user id의 일치 여부를 처리합니다.
-                    if (result.match) {
-                        // post id와 user id가 일치하는 경우
-                        edit_delete_Button.style.display = 'inline-block'
-                    } else {
-                        // post id와 user id가 일치하지 않는 경우
-                        edit_delete_Button.style.display = 'none'
-                    }
-                })
-                .catch(error => console.error('Error:', error)); // 오류가 발생한 경우 콘솔에 오류를 출력합니다.
+                // 서버에서 받은 결과를 확인하여 post id와 user id의 일치 여부를 처리합니다.
+                if (postuserId == userId) {
+                    // post id와 user id가 일치하는 경우
+                    edit_delete_Button.style.display = 'inline-block'
+                } else {
+                    // post id와 user id가 일치하지 않는 경우
+                    edit_delete_Button.style.display = 'none'
+                }
             } else {
                 // 로그아웃 상태인 경우
                 loginButton.style.display = 'inline-block'; // 로그인 버튼을 표시합니다.
