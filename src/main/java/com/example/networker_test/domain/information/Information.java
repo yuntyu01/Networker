ead6c4bfc0b6eca58fe96d0350c9dbb4073fd709
yuntyu01 <yuntyu01@gmail.com>
2024-08-05 22:55:10 +0900

    #63 홈화면 정보게시판 제목 연결

---
+++ src/main/java/com/example/networker_test/domain/information/Information.java
@@ -0,0 +1,30 @@
+package com.example.networker_test.domain.information;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Information {
+    @Id
+    private String title;
+    private String url;
+    @Column(length = 500)
+    private String content;
+    private String image;
+
+    public Information(String title, String url, String content, String image) {
+        this.title = title;
+        this.url = url;
+        this.content = content;
+        this.image = image;
+    }
+
+    protected Information() {
+
+    }
+}
