0a75972f6a7f1e5e819cc1b98094347b6e969888
MystyS2 <kkmmhh7478@gmail.com>
2024-08-06 22:44:18 +0900

    #247 버그 수정 5차시도

--- src/main/resources/static/src/js/board.js
+++ src/main/resources/static/src/js/board.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const profileIcon = document.querySelector('.profile-icon');
     const postButton = document.querySelector('.bottom-btn-box .write-post-button')
 
-    // 로그인 상태 확인 함수(로그인 여부에 따라 헤더 요소 변경)
+    // 로그인 상태 확인 함수
     const checkLoginStatus = () => {
         fetch('/board', {
             method: 'GET',
@@ -13,19 +13,38 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(response => response.json())
             .then(data => {
                 if (data.loggedIn) {
-                    loginButton.style.display = 'none';
-                    signupButton.style.display = 'none';
-                    profileIcon.style.display = 'inline-block';
+                    // 로그인 했을때
                     postButton.style.display = 'inline-block';
-                } else {
-                    loginButton.style.display = 'inline-block';
-                    signupButton.style.display = 'inline-block';
-                    profileIcon.style.display = 'none';
+
+                    if (window.innerWidth > 1090) {
+                        loginButton.style.display = 'none';
+                        signupButton.style.display = 'none';
+                        profileIcon.style.display = 'inline-block';
+                    } else {
+                        loginButton.style.display = 'none';
+                        signupButton.style.display = 'none';
+                        profileIcon.style.display = 'inline-block';
+                    }
+                } else { //로그인안했을때
                     postButton.style.display = 'none';
+
+                    if( window.innerWidth > 1090){
+                        loginButton.style.display = 'inline-block';
+                        signupButton.style.display = 'inline-block';
+                        profileIcon.style.display = 'none';
+                    }
+                    else{
+                        loginButton.style.display = 'none';
+                        signupButton.style.display = 'none';
+                        profileIcon.style.display = 'none';
+                    }
                 }
             })
             .catch(error => console.error('Error:', error));
     };
+
+    // 윈도우 크기 변경 시 요소 가시성 조정
+    window.addEventListener('resize', checkLoginStatus);
 
     // 페이지 로드 시 로그인 상태 확인
     checkLoginStatus();
